e7ec0ac187f6221b192a564f03e0c28d3a3c92db
calibpadao07 <calibpadao@gmail.com>
2026-04-17 17:37:13 +0800

    Refactor login.php for improved readability

--- login.php
+++ login.php
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         header("Location: index.php");
         exit();
     } else {
-        $error = "Invalid username or password.";
+        $error = "Invalid username or password!";
     }
 }
 ?>
